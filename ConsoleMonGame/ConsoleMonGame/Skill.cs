ca9c80cc1c5699dbc5e63fc12dc6f0352dd443f3
kayra <kayrakai8@gmail.com>
2024-05-13 10:47:51 +0200

    halfcourt

---
+++ ConsoleMonGame/ConsoleMonGame/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMonGame
+{
+    internal class Skill
+    {
+        int damage = 43;
+        int energyCost = 34;
+        string name = "half court";
+    }
+}
